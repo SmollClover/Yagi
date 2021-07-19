bf8078e178b23e83e3bc166d803f44d9613bca22
Lars Belitz <39840298+SmollClover@users.noreply.github.com>
2021-07-19 13:36:46 +0200

    🎓 Erste funktionierende Version

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 require('dotenv').config();
 
-const { Client, MessageEmbed } = require('discord.js');
+const { Client } = require('discord.js');
 const client = new Client();
 
 // ~~ Startup ~~
 client.login(process.env.TOKEN).then();
+const guilds = {};
+const lastSize = {};
+const timeBetweenJoin = 1000;
+const amountOfMembersToJoin = 10;
 
 try {
 	for (let i = 0; i < process.stdout.getWindowSize()[1]; i++) {
 		console.log('\r\n');
 	}
-} catch (_) {}
+} catch (_) {
+}
 
 // ~~ Client Events ~~
-client.on('error', (err) => {
+client.on('error', err => {
 	console.log(err);
 });
 
-client.on('warn', (info) => {
+client.on('warn', info => {
 	console.log(info);
 });
 
@@ -29,16 +34,51 @@ client.on('reconnecting', () => {
 	console.log('Reconnecting to Websocket');
 });
 
+client.on('rateLimit', info => {
+	if (!process.env.PROD) console.log('RateLimit: ' + info.timeout + ' ' + info.path + ' ' + info.limit);
+});
+
 client.once('ready', () => {
 	console.log('Finished Booting');
 });
 
-client.on('rateLimit', (info) => {
-	if (!process.env.PROD) {
-		console.log(
-			'RateLimit: ' + info.timeout + ' ' + info.path + ' ' + info.limit
-		);
+client.on('guildMemberAdd', member => {
+	try {
+		guilds[member.guild.id].size;
+	} catch (_) {
+		guilds[member.guild.id] = new Set();
 	}
-});
 
-if (!process.env.PROD) console.log('Dev Mode is Active');
+	guilds[member.guild.id].add(member);
+	lastSize[member.id] = guilds[member.guild.id].size;
+	setTimeout(() => {
+		if (guilds[member.guild.id].size !== lastSize[member.id]) {
+			delete lastSize[member.id];
+			return;
+		}
+		delete lastSize[member.id];
+		if (guilds[member.guild.id].size <= (amountOfMembersToJoin - 1)) {
+			delete guilds[member.guild.id];
+			return;
+		}
+
+		try {
+			guilds[member.guild.id].forEach(m => {
+				m.ban({ reason: 'Yagi | Raid erkannt' }).then(() => {
+					member.guild.channels.cache.forEach(c => {
+						if (c.type !== 'text') return;
+						c.messages.fetch({ limit: 50, force: true }).then(messages => {
+							let msgs = messages.filter(msg => msg.author.id === m.id);
+							msgs.forEach(msg => {
+								msg.delete();
+							});
+						});
+					});
+				});
+			});
+			delete guilds[member.guild.id];
+		} catch (_) {
+			delete guilds[member.guild.id];
+		}
+	}, timeBetweenJoin);
+});
